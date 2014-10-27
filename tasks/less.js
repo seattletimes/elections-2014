5696c4041adb3ab66f48aefa5ef0798e21ef5afe
Thomas Wilburn <twilburn@seattletimes.com>
2014-10-27 11:49:39 -0700

    Started widget work.

--- tasks/less.js
+++ tasks/less.js
@@ -6,26 +6,38 @@ Run the LESS compiler against seed.less and output to style.css.
 
 module.exports = function(grunt) {
 
-  var fs = require("fs");
+  var async = require("async");
   var less = require("less");
   
   var options = {
-    paths: ["src/css"],
-    filename: "seed.less"
+    paths: ["src/css"]
   };
+
+  var files = ["style.less", "widget.less"];
   
   grunt.registerTask("less", function() {
     
     var c = this.async();
-    
-    var seed = grunt.file.read("src/css/seed.less");
-    
-    var parser = new less.Parser(options);
-    parser.parse(seed, function(err, tree) {
-      var css = tree.toCSS();
-      grunt.file.write("build/style.css", css);
-      c();
+
+    var paths = grunt.file.expandMapping(files, "./build", {
+      ext: ".css",
+      cwd: "src/css"
     });
+
+    async.each(paths, function(path, done) {
+
+      var filename = path.src.pop();
+
+      var seed = grunt.file.read(filename);
+      
+      var parser = new less.Parser(options);
+      parser.parse(seed, function(err, tree) {
+        var css = tree.toCSS();
+        grunt.file.write(path.dest, css);
+        done();
+      });
+    }, c);
+    
     
   });
 
